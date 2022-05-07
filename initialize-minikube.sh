76566f771358141c4631adea637db7f1755f4ddf
iomegak12 <jd.ramkumar@gmail.com>
2022-05-07 22:42:51 +0530

    committed

---
+++ initialize-minikube.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $(date)
+
+sudo apt-get update
+
+sudo apt-get install nano jq dpkg-dev -y
+
+curl -sSL https://get.docker.com | sudo bash -E -
+
+sudo snap install kubectl --classic
+
+sudo usermod -aG docker ubuntu
+
+wget https://storage.googleapis.com/minikube/releases/latest/minikube_latest_$( dpkg-architecture -q DEB_HOST_ARCH ).deb
+
+sudo apt-get install --yes --no-install-recommends -o Debug::pkgProblemResolver=yes ./minikube_latest_$( dpkg-architecture -q DEB_HOST_ARCH ).deb
+
+minikube start --driver=docker
+
